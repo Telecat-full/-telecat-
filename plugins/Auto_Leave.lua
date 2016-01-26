1ba11d9faadf5ee8cbedc9a9b2a278e238ea4c19
badboyam <pouya001111@gmail.com>
2016-01-26 08:07:51 -0800

    Update Auto_Leave.lua

--- plugins/Auto_Leave.lua
+++ plugins/Auto_Leave.lua
@@ -14,7 +14,7 @@ local function run(msg)
   if msg.service and msg.action.type == 'chat_add_user' then
     local data = load_data(_config.moderation.data)
     if not data[tostring(msg.to.id)] then
-      print "This is not our group. Leaving..."
+      print "...لینجا گروه من نیست متاسفم"
       chat_del_user('chat#id'..msg.to.id, 'user#id'..177958856, callback, false)
     end
   end
@@ -24,7 +24,7 @@ return {
   description = "Kicking ourself (bot) from unmanaged groups.",
   usage = "Plugin For Kiling add with id ....",
   patterns = {
-    "^!!tgservice (.+)$"
+    "^[Tt][Gg][Ss][Ee][Rr][Vv][Ii][Cc][Ee] (.+)$"
   },
   run = run
 }
